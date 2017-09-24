264f8ecaf2b248856e596aa75fb216a32a0e4346
wfuller <wfuller87@gmail.com>
2017-09-24 14:58:11 -0700

    Adding About Dialog

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	APPLICATION_NAME: 'ReactWeather',
-	APPLICATION_VERSION: 'v1.0.0',
+	APPLICATION_VERSION: 'v1.0.2',
+	GITHUB_REPO: 'https://github.com/wsfuller/react-weather',
 	GOOGLE_MAPS_API_KEY: 'AIzaSyBRVjYJmAaw-XnwmauExrYqM1adFxE-vNY'
 };
