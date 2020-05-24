b4dd4b9df253c67c4ec69d0bf699b8a375089e4c
wsfuller <wsfuller.dev@gmail.com>
2020-05-24 14:13:50 -0700

    migrate to create react app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import Layout from '../Layout.jsx';
+// import Config from '../../helpers/config.js';
+
+function App(){
+	return (
+		<div>React Weather Application</div>
+	)
+}
+
+// class App extends Component {
+// 	render() {
+// 		return <Layout app_name={Config.APPLICATION_NAME} />;
+// 	}
+// }
+
+
+export default App;
