d0d3aa23e3eaf7b67d072bda18567dbf9d257968
Steve <wsfuller.dev@gmail.com>
2020-06-06 21:31:09 -0700

    Clean up AppBar styles

--- src/components/AppBar/AppBar.styles.js
+++ src/components/AppBar/AppBar.styles.js
@@ -1,9 +1,6 @@
 const AppBarStyles = (theme) => {
   return {
     root: {
-      // display: 'flex',
-      // alignItems: 'center',
-      // justifyContent: 'space-between',
       width: '100%',
       padding: `${theme.spacing.s1} ${theme.spacing.m}`,
       backgroundColor: theme.palette.neutralLight,
