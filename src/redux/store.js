42046946540322a87382395924ec7678367e7ed1
Steve <wsfuller.dev@gmail.com>
2020-06-07 22:50:55 -0700

    Updating redux promise middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import promise from 'redux-promise-middleware';
+import { createPromise } from 'redux-promise-middleware';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 
-const middleware = applyMiddleware(promise(), thunk, createLogger());
+const middleware = applyMiddleware(createPromise(), thunk, createLogger());
 
 export default createStore(reducer, composeWithDevTools(middleware));
