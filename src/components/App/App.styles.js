aa69f2c837f9688a56d2253ca5a01844e4b4c7c7
Steve <wsfuller.dev@gmail.com>
2020-06-28 21:32:05 -0700

    Updating App styles flex prop

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -4,7 +4,7 @@ export const styles = (theme) => {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: 'space-between',
+      justifyContent: 'start',
       width: '100%',
       height: '100vh',
       color: theme.semanticColors.bodyText,
