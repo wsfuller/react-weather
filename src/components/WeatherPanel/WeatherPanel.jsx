999fcf13bb103414d7c722ac418932b57b47f4aa
Steve <wsfuller.dev@gmail.com>
2020-07-19 12:16:22 -0700

    Updating WeatherPanel
    Using new DetailBar component

--- src/components/WeatherPanel/WeatherPanel.jsx
+++ src/components/WeatherPanel/WeatherPanel.jsx
@@ -1,30 +1,77 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
-import { classNamesFunction, styled, Separator, Text } from '@fluentui/react';
+import {
+  classNamesFunction,
+  styled,
+  Icon,
+  Spinner,
+  SpinnerSize,
+  Text,
+} from '@fluentui/react';
 
 import WeatherPanelStyles from './WeatherPanel.styles';
-import WeatherDetail from '../WeatherDetail';
+import { DetailBar } from '../WeatherDetail';
+
+import {
+  convertTemperature,
+  convertHumidity,
+  convertClouds,
+  convertVisibility,
+} from '../../utils/conversions';
 
 const getClassNames = classNamesFunction();
 
 function WeatherPanel({ theme }) {
   const classes = getClassNames(WeatherPanelStyles, theme);
+  const { weather, loading, error } = useSelector((state) => state.weather);
+  const { temperatureScale } = useSelector((state) => state.userPreferences);
 
-  return (
-    <div className={classes.root}>
-      <Text variant="xxLarge" className={classes.location}>
-        San Francisco, CA
-      </Text>
-      <Text variant="mega" className={classes.currentTemperature}>
-        55&deg;F
-      </Text>
-      <Separator alignContent="start">Wind</Separator>
-      <WeatherDetail />
-      <div className={classes.lowHighTemperature}>
-        55&deg;F =================== 60&deg;F
-      </div>
-    </div>
-  );
+  let content;
+
+  if (loading) {
+    content = <Spinner size={SpinnerSize.large} />;
+  } else {
+    content = (
+      <Fragment>
+        <Text variant="xxLarge" className={classes.location}>
+          <Icon iconName="MapPin" className={classes.locationIcon} />
+          {weather.name}, CA
+        </Text>
+        <Text variant="mega" className={classes.currentTemperature}>
+          <span
+            dangerouslySetInnerHTML={{
+              __html: convertTemperature(weather?.main?.temp, temperatureScale),
+            }}
+          />
+        </Text>
+        <DetailBar
+          textLeft={convertTemperature(
+            weather?.main?.temp_min,
+            temperatureScale
+          )}
+          textRight={convertTemperature(
+            weather?.main?.temp_max,
+            temperatureScale
+          )}
+          description="Low/High"
+          fill={1}
+        />
+        <DetailBar
+          textLeft={`${weather.main.humidity}&#37;`}
+          description="Humidity"
+          fill={convertHumidity(weather.main.humidity)}
+        />
+        <DetailBar
+          textLeft={`${weather.clouds.all}&#37;`}
+          description="Clouds"
+          fill={convertClouds(weather.clouds.all)}
+        />
+      </Fragment>
+    );
+  }
+
+  return <div className={classes.root}>{content}</div>;
 }
 
 export default styled(WeatherPanel, WeatherPanelStyles);
--- src/components/WeatherPanel/WeatherPanel.styles.js
+++ src/components/WeatherPanel/WeatherPanel.styles.js
@@ -1,19 +1,25 @@
 const WeatherPanelStyles = (theme) => {
-  console.log('theme: ', theme);
   return {
     root: {
       position: 'absolute',
       left: 0,
       display: 'flex',
       flexDirection: 'column',
-      maxWidth: '30vw',
+      width: '30vw',
+      maxWidth: 400,
       height: '100%',
       padding: `${theme.spacing.l1} ${theme.spacing.m}`,
       backgroundColor: theme.palette.neutralLighterAlt,
+      borderTop: `1px solid ${theme.palette.accent}`,
     },
     location: {
       marginTop: 0,
       fontWeight: theme.FontWeights.light,
+      textAlign: 'right',
+    },
+    locationIcon: {
+      marginRight: theme.spacing.s2,
+      fontSize: '70%',
     },
     currentTemperature: {
       marginTop: 0,
